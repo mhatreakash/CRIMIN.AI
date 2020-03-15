c7f8ec9fdf51bba301b47218116c9d94d44b4fe1
Anuradha <anudate97@gmail.com>
2020-03-15 12:17:27 +0530

    basic UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { Component } from "@angular/core";
+import { AngularFirestore } from "angularfire2/firestore";
+import { NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
   userDoc: any;
 
-  constructor(private fireStore: AngularFirestore) {
-    fireStore.collection("test").valueChanges().subscribe(data =>{
-      this.userDoc = data;
-    });
+  constructor(
+    private fireStore: AngularFirestore,
+    public NavCtrl: NavController
+  ) {
+    fireStore
+      .collection("test")
+      .valueChanges()
+      .subscribe(data => {
+        this.userDoc = data;
+      });
   }
 
+  toSignUp1() {
+    this.NavCtrl.navigateForward("/sign-up");
+  }
 }
