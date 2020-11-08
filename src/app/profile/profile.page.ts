aaa79421900995bf8c42afb14c24900f7d464b79
Anuradha <anudate97@gmail.com>
2020-11-08 12:10:04 +0530

    page changes 1

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from "@ionic/angular";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router, public NavCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  EditProfile() {
+    this.NavCtrl.navigateForward("/edit-profile");
+  }
+
+  ChangePassword() {
+    this.NavCtrl.navigateForward("/change-password");
   }
 
 }
