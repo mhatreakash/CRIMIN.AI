bf7e2639fe2820bc1ded5b573903b0dc80206f27
Anuradha <anudate97@gmail.com>
2020-11-11 10:21:53 +0530

    change and additions 1

--- src/app/edit-profile/edit-profile.page.ts
+++ src/app/edit-profile/edit-profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(public action : ActionSheetController,public alertController :AlertController) { }
+
+  async ActionSheet() {
+    const actionSheet = await this.action.create({
+      header: 'Albums',
+      buttons: [{
+        text: 'Gallery',
+        icon:"image",
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+   }
+ 
+
 
   ngOnInit() {
   }
